fdd18be2e1abbadfd9585c733d101b295b05407a
Shaik077 <tshaikvali@gmail.com>
2020-05-19 23:13:36 +0530

    casestatementwage

---
+++ casestatementwage.sh
@@ -0,0 +1,19 @@
+#!/bin/bash -x
+
+fulltimewage=1;
+parttimewage=2;
+employeesalaryperhour=20;
+employeecheck=$(($RANDOM%3));
+case $employeecheck in
+ $fulltimewage)
+      employeehour=8
+             ;;
+ $parttimewage)
+    
+       employeehour=5
+                 ;;
+ *)
+      employeehour=0;
+esac
+ salary=$(( $employeehour * $employeesalaryperhour))
+echo $salary

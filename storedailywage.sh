dba073d22cd0eb98f531ddb0106d24d575bee87b
Shaik077 <tshaikvali@gmail.com>
2020-05-21 22:34:05 +0530

    storedailywage

---
+++ storedailywage.sh
@@ -0,0 +1,37 @@
+#!/bin/bash -x
+
+isparttime=1
+isfulltime=2
+maxhrsinmonth=4
+empsrateperhr=20
+numworkingday=20
+
+totalemphrs=0
+totalworkingdays=0
+
+function getworkhrs() {
+  local $empcheck=$1
+    case $empcheck in
+      $isfulltime)
+          emphrs=8
+           ;;
+        $parttime)
+             emphrs=4
+             ;;
+         *)
+             emphrs=0
+               ;;
+esac
+echo $empshrs
+
+}
+while [[ $totalemphrs -lt $maxhrsinmonth && $totalworkingdays -lt $numworkingday ]]
+do
+ ((totalworkingdays++))
+ empcheck=$((RANDOM%3));
+ emphrs="$( getworkhrs $empcheck )"
+  totalemphrs=${($totalemphrs+$emphrs)}
+ dailywage[$totalworkingdays]=(($emphrs*$emprateperhrs))
+done
+totalsalary=${($totalemphrs*$empsrateperhr))
+echo ${dailwage[@]}

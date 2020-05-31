243a6e5cfc9e9a82635d782de2957b40febc5e54
Shaik077 <tshaikvali@gmail.com>
2020-05-31 21:14:29 +0530

    javaemployeewage

---
+++ javaemployeewage.java
@@ -0,0 +1,11 @@
+public class javaemployeewage{ 
+              public static void main(String[] args)
+               {
+                   int fulltime=1;
+                   double empcheck=Math.floor(Math.random() * 10) % 2; 
+                 if (empcheck ==  fulltime)
+                            System.out.print("present");
+                 else 
+                          System.out.print("absent");
+   }
+}

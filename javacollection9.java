a9b8367ba3c4d81c1c603f2a29ee09e6c84f120c
Shaik077 <tshaikvali@gmail.com>
2020-06-05 23:29:14 +0530

    javacollection9

---
+++ javacollection9.java
@@ -0,0 +1,21 @@
+public class javaUc9{
+                 public static final int fulltime=1;
+                  public static final int parttime=2;
+
+                  public static final String company;
+                  public static final int emprateperhour;
+                  public static final int numberofdays;
+                  public static  final int maxhrsinmonth;
+                  public static int javaUc9(String company, int emprateperhour, int numberofdays; int maxhrsinmonth){
+                   this.company=company;
+                   this.emprateperhour=emprateperhour;
+                   this.numberofdays=numberofdays;
+                   this.maxhrsinmonth=maxhrsinmonth;
+               }
+            public void settotalempwage(int totalempwage){
+                     this. totalempwage=totalempwage;
+                   }
+                      public String toString{
+                     returen "total emp wage for company: " +company+" is: "+totalwage;
+}
+}
